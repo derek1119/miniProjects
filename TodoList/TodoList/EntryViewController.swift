36bf181d90a9cbb6809001e574c462ff161ec62d
nyokki1119 <ghdtjrgus333@naver.com>
2021-05-19 18:27:39 +0900

    todolist

---
+++ TodoList/TodoList/EntryViewController.swift
@@ -0,0 +1,44 @@
+//
+//  EntryViewController.swift
+//  TodoList
+//
+//  Created by Sh Hong on 2021/05/19.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    //delegate는 프로토콜로 구현이 된다. 프로토콜이란 일종의 약속, 규약이라고 생각하면 된다. 개발자는 프로토콜 내에 정의되어 있는 요소들, 즉 함수 프로토타입을 가지고 원하는 기능을 구현하면 된다.
+    
+    
+    //task 입력을 위한 textfield outlet 연결
+    @IBOutlet var field: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.delegate = self
+        //여기서 위 의미는 텍스트 필드의 일은 내가 할게 하는 의미가 된다. 여기서 '내'는 현재 클래스인
+        //EntryViewController이다. 즉, 텍스트 필드에 어떤 일(이벤트)가 일어나면 EntryViewController가 처리한다는 뜻이다.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
+        //navigationItem 중 rightbarButton에 save 버튼을 만들어서 done이라는 어떤 이벤트와 이전화면으로 돌아가게 하는 스타일로 표현했다. target은 액션 메세지를 받는 객체인데 self라는 것은 (아마도?)버튼 자체에서 액션 메세지를 받는다는 의미인 것 같다. #selector란 함수를 직접 지정하는 기능을 가진 일종의 함수 선택자이다. 본래 object-c에서 클래스 메소드의 이름을 가르키는데 사용되는 참조타입이다.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        saveTask()
+        
+        return true
+    }
+    //텍스트 필드에 무슨 일이 발생하고 반환값이 있다라는 함수, 델리게이트를 '구현'하는 과정이다.
+    //'사용자가 텍스트필드에 어떤 일을 하고 리턴될 것이다.'의 의미, 여기서 return은 엔터
+    
+    @objc func saveTask() {
+        
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        
+    }
+    //#selector에서 사용되려면 @objc func이 되어야한다.
+}
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Tableview outlet 설정, IBOutlet은 non-optional이다.
+    @IBOutlet var tableView : UITableView!
+    
+    //업무에 대한 스트링 타입의 빈 배열 생성 -> 셀로 보냄
+    var tasks = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Tasks"
+        // 현재 저장된 모든 tasks를 불러오기.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func addButtonTapped() {
+        
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        //위 코드는 스토리보드 파일로부터 데이터를 불러와서 viewController를 생성하고 메모리 공간의 allocation을 의미한다.
+        //storyboard안에 기입한 storyboardID를 가지고 스토리보드 데이터를 초기화 해 viewController를 만든다.
+        //instantiateViewController는 새로운 뷰로 넘어갈 때 사용해야 한다. 
+        vc.title = "New Task"
+        navigationController?.pushViewController(vc, animated: true)
+        // 위 방식은 네비게이션컨트롤러를 사용하여 화면 전환하는 방법이다.
     }
 
 
 }
+//셀과 상호작용하는 tableview의 델리게이트
+extension ViewController : UITableViewDelegate {
+    
+    //셀이 선택된 직후 작동하는 함수
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //셀이 선택되면 애니메이션 작동
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+//셀에 표현될 데이터 소스를 상호작용하는 extention, 즉 테이블뷰와 관련된 데이터를 제공하기 위함
+extension ViewController : UITableViewDataSource {
+    
+    //tableViewDataSource에서 무조건 가지고 있어야하는 것 -> 섹션 안에 있는 row(행)의 갯수를 리턴해주는 함수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    //셀을 만들어낼 때는 아래 코드가 한번만 불리게 되면서 모든 셀이 다 만들어지는 것이 아니라 이 함수는 cell하나를 리턴하기 때문에 cell이
+    //만들어질때마다 아래 함수가 호출이 된다.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //dequeueReusableCell이란 지정된 재사용 식별자에 대한 재사용 가능한 테이블 뷰 셀 객체를 반환하고 이를 테이블에 추가
+        //지정된 (재사용) 식별자란 withIdentifier가 될 것이며 string형 identifier는 재사용할 객차를 나타낸다.
+        //여기서 for란 indexPath인데 셀의 위치를 알려주는 것
+        //데이터 소스는 셀에 대한 요청이 있을 때 이 정보를 수신하며 이를 전달해야한다. 이 방법은 인덱스 경로를 사용하여 테이블뷰에서 셀의 위치를 기반으로 추가 구성을 수행한다.
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        //indexPath란 행을 식별하는 인덱스 경로이다. indexPath는 [?, row]의 값을 갖는다.
+        //앞의 ?는 셀의 섹션을 의미하고 row는 해당 섹션의 row번째 행을 말한다.
+        
+        return cell
+    }
+    
+    
+}
