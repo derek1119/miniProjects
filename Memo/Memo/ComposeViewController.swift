df2c2e578a7f45a9408afd7ad2b6e28c9aeaf96d
nyokki1119 <ghdtjrgus333@naver.com>
2021-05-21 14:53:21 +0900

    저장 구현

--- Memo/Memo/ComposeViewController.swift
+++ Memo/Memo/ComposeViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    @IBOutlet var memoTextView: UITextView!
+    //memoTextView 아울렛으로 연결 -> 이를 컨트롤하기 위함.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +21,21 @@ class ComposeViewController: UIViewController {
     @IBAction func cancelButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
         //화면을 닫는 메소드
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let memo = memoTextView.text, memo.count > 0 else {
+            alert(message: "메모를 입력하세요.")
+            return
+        }
+        // memo.count > 0 인 이유는 텍스트가 입력되었는지 확인하는 것이고 만일 어떤 입력도 받지 않았다면 alert를 하고 return, 만일 텍스트가 입력되었다면 가드문 다음의 코드가 실행된다.
+        
+        let newMemo = Memo(content: memo)
+        //memo를 Memo 모델의 컨텐트로 상수화 시키고
+        Memo.dummyMemoList.append(newMemo)
+        //이 텍스트를 dummyMemoList에 추가함.
+        
+        dismiss(animated: true, completion: nil)
     }
     
     /*
---
+++ Memo/Memo/UIViewController+Alert.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Alert.swift
+//  Memo
+//
+//  Created by Sh Hong on 2021/05/21.
+//
+
+import UIKit
+
+extension UIViewController {
+    //UIViewController 전반에 사용할 수 있도록 extension으로 구현
+    func alert(title : String = "알림", message : String ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // UIAlertController를 이용하여 alert 객체 만듬
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        //확인 객체 만듬 -> 확인 선택 후 어떤 이벤트도 일어나지 않으니 핸들러는 nil
+        alert.addAction(okAction)
+        //action을 만들었으면 alert에 추가해야함.
+        
+        present(alert, animated: true, completion: nil)
+        //마지막 present를 이용해서 알림을 보여줘야함. 
+    }
+    //알림을 위한 함수 구현
+}
