0ee5193ff121c6bc1900cef3ffabc03e38c1ff80
nyokki1119 <ghdtjrgus333@naver.com>
2021-06-01 16:40:04 +0900

    메모 프로젝트 생성

---
+++ SHMemo/SHMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHMemo
+//
+//  Created by Sh Hong on 2021/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
