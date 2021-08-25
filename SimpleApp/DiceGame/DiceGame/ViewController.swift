c325d069543ad834c69cc8e2cbadcec2039165ce
nyokki1119 <ghdtjrgus333@naver.com>
2021-08-25 23:53:19 +0900

    Dice Game

---
+++ SimpleApp/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Sh Hong on 2021/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
