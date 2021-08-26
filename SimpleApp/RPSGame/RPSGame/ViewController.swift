fba2d97b3e7a17b476faeb53a8e0975da994dfaa
nyokki1119 <ghdtjrgus333@naver.com>
2021-08-26 10:50:44 +0900

    RPS Game 뷰 구성

---
+++ SimpleApp/RPSGame/RPSGame/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RPSGame
+//
+//  Created by Sh Hong on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rpsBtnTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectBtnTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetBtnTapped(_ sender: UIButton) {
+    }
+    
+}
+
