6b8799ebc282aab515f82f41364882c6ed58f00f
nyokki1119 <ghdtjrgus333@naver.com>
2021-10-21 19:39:57 +0900

    select image view 기능 구현

--- instagramClone/instagramClone/Controller/UploadPost/SelectImageViewController.swift
+++ instagramClone/instagramClone/Controller/UploadPost/SelectImageViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 private let reuseIdentifier = "SelectPhotoCell"
 private let headerIdentifier = "SelectPhotoHeader"
@@ -13,6 +14,10 @@ private let headerIdentifier = "SelectPhotoHeader"
 class SelectImageViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     // MARK: - Properties
+    
+    var images = [UIImage]()
+    var assets = [PHAsset]()
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,9 @@ class SelectImageViewController: UICollectionViewController, UICollectionViewDel
         
         // configure nav buttons
         configureNavigationButton()
+        
+        // fetch photos
+        fetchPhotos()
     }
     
     // MARK: - UICollectionViewFlowLayout
@@ -52,19 +60,53 @@ class SelectImageViewController: UICollectionViewController, UICollectionViewDel
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return images.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? SelectPhotoCell else { return UICollectionViewCell() }
         
+            cell.photoImageView.image = images[indexPath.row]
+        
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        self.selectedImage = images[indexPath.row]
+        
+        collectionView.reloadData()
+        
+        let indexPath = IndexPath(item: 0, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .bottom, animated: true)
+        
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
         guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as? SelectPhotoHeader else { return UICollectionReusableView()}
         
+        // 높은 품질의 이미지 얻어오는 방법
+        if let selectedImage = selectedImage {
+           
+            // index selected image
+            if let index = images.firstIndex(of: selectedImage) {
+                
+                // asset associated with selected image
+                let selectedAsset = self.assets[index]
+                
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 600, height: 600)
+                
+                // request image
+                imageManager.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .default, options: nil) { image, info in
+                    
+                    header.photoImageView.image = image
+                    
+                }
+            }
+            
+        }
         return header
     }
     
@@ -83,4 +125,75 @@ class SelectImageViewController: UICollectionViewController, UICollectionViewDel
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
     }
+    
+    func getAssetFetchOptions() -> PHFetchOptions {
+        let options = PHFetchOptions()
+        
+        // fetch limit
+        options.fetchLimit = 30
+        
+        // sort photos by date
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+        
+        // set sort descriptor for options
+        options.sortDescriptors = [sortDescriptor]
+        
+        return options
+        
+    }
+    
+    func fetchPhotos() {
+        
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: getAssetFetchOptions())
+        
+        // fetch images on background thread
+        DispatchQueue.global(qos: .background).async {
+            
+            // enumerate objects -> 객체와 인덱스를 함께,
+            allPhotos.enumerateObjects { asset, count, stop in
+                
+                print("몇 번째 사진일까요? \(count)")
+                
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 200, height: 200)
+                let options = PHImageRequestOptions()
+                // 이미지 데이터가 준비되거나 오류가 발생할 때까지 호출 스레드를 차단
+                options.isSynchronous = true
+                
+                // request image representation for specified asset
+                imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options) { image, info in
+                    if let image = image {
+                        
+                        // append image to data source
+                        self.images.append(image)
+                        
+                        // append asset to data source
+                        self.assets.append(asset)
+                        
+                        // set selected image with first image
+                        if self.selectedImage == nil {
+                            self.selectedImage = image
+                        }
+                        
+                        // reload collection view with images once count has completed -> 패치가 끝났는지 확인하는 것
+                        if count == allPhotos.count - 1 {
+                            
+                            // reload collection view on main thread
+                            DispatchQueue.main.async {
+                                self.collectionView.reloadData()
+                            }
+                        }
+                        
+                        
+                    }
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    
 }
